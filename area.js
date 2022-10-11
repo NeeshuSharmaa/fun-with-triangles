d7c8815dc3738b68ca97e333f558521c5b172eb5
NeeshuSharmaa <neeshusharmaa@gmail.com>
2022-10-11 17:06:00 +0530

    --

---
+++ area.js
@@ -0,0 +1,14 @@
+const triangleSides = document.querySelectorAll(".triangle-sides-input");
+const output = document.querySelector("#output-box");
+const areaBtn = document.querySelector("#area-btn")
+
+
+function areaOfTriangle() {
+    var area=1/2*(Number(triangleSides[0].value)*(Number(triangleSides[1].value)));
+    
+    output.innerHTML = `The area is <div class="area-style">` + area + "</div>";
+}
+
+
+
+areaBtn.addEventListener("click", areaOfTriangle);
--- index.js
+++ index.js
@@ -1,22 +1,40 @@
 const inputs=document.querySelectorAll(".angle-input");
-const isTriangleBtn=document.querySelector("#is-trianle-btn");
+const isTriangleBtn=document.querySelector("#is-triangle-btn");
 const outputE1=document.querySelector("#output");
 
 
-function calculateSumOfAngles(angle1,angle2, angle3) {
-    const sumOfAngles =angle1+angle2+angle3;
-    return sumOfAngles;
+function calculateSumOfAngles(angle1,angle2,angle3) {
+    const sum = angle1+angle2+angle3;
+    return sum;
 }
+
 function isTriangle() {
-    const sumOfAngles = calculateSumOfAngles(Number(inputs[0].value),Number(inputs[1].value),Number(inputs[2].value ));
-    if(sumOfAngles===180) {
-        outputE1.innerText="Yay, The angles form a triangle";
-        // console.log("Yay, The angles form a triangle")
+    angle1=inputs[0].value
+    angle2=inputs[1].value
+    angle3=inputs[2].value
+    if (inputs[0].value && inputs[1].value && inputs[2].value) {
+        if (String(Number(angle1)) != "NaN" && String(Number(angle2)) != "NaN" && String(Number(angle3)) != "NaN") {
+            console.log(Number(angle1));
+            const sumOfAngles = calculateSumOfAngles(Number(angle1), Number(angle2),Number (angle3));
+            if(sumOfAngles===180) {
+                outputE1.innerText="Yay, The angles form a triangle";
+                // console.log("Yay, The angles form a triangle")
+            }
+            else {
+                outputE1.innerText="Oh oh! The angles don't form a triangle";
+                // console.log("Oh oh! The angles don't form a triangle");
+            }
+        }
+        else {
+            outputE1.innerText="Type the numbers!!"
+        }
+        
     }
     else {
-        outputE1.innerText="Oh oh! The angles don't form a triangle";
-        // console.log("Oh oh! The angles don't form a triangle");
+        outputE1.innerText="Enter all the values"
     }
 }
 
-isTriangleBtn.addEventListener("click", isTriangle) 
+isTriangleBtn.addEventListener("click", isTriangle);
+// isTriangleBtn.innerText=2;
+// document.body.innerText=2;
